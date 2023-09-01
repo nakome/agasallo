6d379cee2775b46bf43864bbf9de971701a52fa0
Moncho Varela <nakome@gmail.com>
2023-09-02 00:10:59 +0200

    Remove aditional & unused properties on Css

--- website/src/components/Code.jsx
+++ website/src/components/Code.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 // https://react-icons.github.io/react-icons/icons?name=bi
-import { BiExpand,BiCollapse } from "react-icons/bi";
+import { BiExpand, BiCollapse } from "react-icons/bi";
 
 import CodeMirror from "@uiw/react-codemirror";
 import { aura } from "@uiw/codemirror-theme-aura";
 import { loadLanguage } from "@uiw/codemirror-extensions-langs";
 
-import { Button,Select } from "./Forms";
+import { Button, Select } from "./Forms";
 
 export default function CodeBlock(props) {
   return (
@@ -22,18 +22,18 @@ export default function CodeBlock(props) {
         />
         <div className="code-header-right">
           <Button onClick={props.expand}>
-              {props.active ? (<BiExpand />) : <BiCollapse />}
+            {props.active ? <BiExpand /> : <BiCollapse />}
           </Button>
         </div>
       </div>
       <div className="code-body">
-          <CodeMirror
-            value={props.content}
-            height="100%"
-            extensions={[loadLanguage(props.type)]}
-            theme={aura}
-            onChange={props.setContent}
-          />
+        <CodeMirror
+          value={props.content}
+          height="100%"
+          extensions={[loadLanguage(props.type)]}
+          theme={aura}
+          onChange={props.setContent}
+        />
       </div>
     </div>
   );
